1dab0b877bdb8517360cba9922e8c681edeaf6e3
rachanakreddy <rachanakreddy@gmail.com>
2024-09-20 00:15:36 -0400

    added vite and solidified app.js file

--- client/audio.js
+++ client/audio.js
@@ -1,17 +1,5 @@
-
-// paulirish.com/2009/log-a-lightweight-wrapper-for-consolelog/
-window.log = function f(){ log.history = log.history || []; log.history.push(arguments); if(this.console) { var args = arguments, newarr; args.callee = args.callee.caller; newarr = [].slice.call(args); if (typeof console.log === 'object') log.apply.call(console.log, console, newarr); else console.log.apply(console, newarr);}};
-// make it safe to use console.log always
-(function(a){function b(){}for(var c="assert,count,debug,dir,dirxml,error,exception,group,groupCollapsed,groupEnd,info,log,markTimeline,profile,profileEnd,time,timeEnd,trace,warn".split(","),d;!!(d=c.pop());){a[d]=a[d]||b;}})
-(function(){try{console.log();return window.console;}catch(a){return (window.console={});}}());
-
-
-/*! Copyright (c) 2013 - Peter Coles (mrcoles.com)
- *  Licensed under the MIT license: http://mrcoles.com/media/mit-license.txt
- */
 (function() {
 
-    // test if we can use blobs
     var canBlob = false;
     if (window.webkitURL && window.Blob) {
         try {
@@ -21,13 +9,6 @@ window.log = function f(){ log.history = log.history || []; log.history.push(arg
     }
 
     function asBytes(value, bytes) {
-        // Convert value into little endian hex bytes
-        // value - the number as a decimal integer (representing bytes)
-        // bytes - the number of bytes that this value takes up in a string
-
-        // Example:
-        // asBytes(2835, 4)
-        // > '\x13\x0b\x00\x00'
         var result = [];
         for (; bytes>0; bytes--) {
             result.push(String.fromCharCode(value & 255));
@@ -67,49 +48,12 @@ window.log = function f(){ log.history = log.history || []; log.history.push(arg
     }, {
         style: {
             wave: function(freq, volume, i, sampleRate, seconds) {
-                // wave
-                // i = 0 -> 0
-                // i = (sampleRate/freq)/4 -> 1
-                // i = (sampleRate/freq)/2 -> 0
-                // i = (sampleRate/freq)*3/4 -> -1
-                // i = (sampleRate/freq) -> 0
                 return Math.sin((2 * Math.PI) * (i / sampleRate) * freq);
-            },
-            squareWave: function(freq, volume, i, sampleRate, seconds, maxI) {
-                // square
-                // i = 0 -> 1
-                // i = (sampleRate/freq)/4 -> 1
-                // i = (sampleRate/freq)/2 -> -1
-                // i = (sampleRate/freq)*3/4 -> -1
-                // i = (sampleRate/freq) -> 1
-                var coef = sampleRate / freq;
-                return (i % coef) / coef < .5 ? 1 : -1;
-            },
-            triangleWave: function(freq, volume, i, sampleRate, seconds, maxI) {
-                return Math.asin(Math.sin((2 * Math.PI) * (i / sampleRate) * freq));
-            },
-            sawtoothWave: function(freq, volume, i, sampleRate, seconds, maxI) {
-                // sawtooth
-                // i = 0 -> -1
-                // i = (sampleRate/freq)/4 -> -.5
-                // i = (sampleRate/freq)/2 -> 0
-                // i = (sampleRate/freq)*3/4 -> .5
-                // i = (sampleRate/freq) - delta -> 1
-                var coef = sampleRate / freq;
-                return -1 + 2 * ((i % coef) / coef);
             }
         },
         volume: {
-            flat: function(data, freq, volume) {
-                return volume * data;
-            },
             linearFade: function(data, freq, volume, i, sampleRate, seconds, maxI) {
                 return volume * ((maxI - i) / maxI) * data;
-            },
-            quadraticFade: function(data, freq, volume, i, sampleRate, seconds, maxI) {
-                // y = -a(x - m)(x + m); and given point (m, 0)
-                // y = -(1/m^2)*x^2 + 1;
-                return volume * ((-1/Math.pow(maxI, 2))*Math.pow(i, 2) + 1) * data;
             }
         }
     });
@@ -121,32 +65,23 @@ window.log = function f(){ log.history = log.history || []; log.history.push(arg
 
         cfg = $.extend({
             channels: 1,
-            sampleRate: 11025, // Hz
-            bitDepth: 16, // bits/sample
+            sampleRate: 11025, 
+            bitDepth: 16, 
             seconds: .5,
-            volume: 20000,//32767,
+            volume: 20000,
             freq: 440
         }, cfg);
 
-        //
-        // Format Sub-Chunk
-        //
-
         var fmtChunk = [
-            'fmt ', // sub-chunk identifier
-            asBytes(16, 4), // chunk-length
-            asBytes(1, 2), // audio format (1 is linear quantization)
+            'fmt ', 
+            asBytes(16, 4), 
+            asBytes(1, 2), 
             asBytes(cfg.channels, 2),
             asBytes(cfg.sampleRate, 4),
             asBytes(cfg.sampleRate * cfg.channels * cfg.bitDepth / 8, 4), // byte rate
             asBytes(cfg.channels * cfg.bitDepth / 8, 2),
             asBytes(cfg.bitDepth, 2)
         ].join('');
-
-        //
-        // Data Sub-Chunk
-        //
-
         var sampleData = DataGenerator(
             cfg.styleFn || DataGenerator.style.default,
             cfg.volumeFn || DataGenerator.volume.default,
@@ -154,14 +89,10 @@ window.log = function f(){ log.history = log.history || []; log.history.push(arg
         var samples = sampleData.length;
 
         var dataChunk = [
-            'data', // sub-chunk identifier
-            asBytes(samples * cfg.channels * cfg.bitDepth / 8, 4), // chunk length
+            'data', 
+            asBytes(samples * cfg.channels * cfg.bitDepth / 8, 4), 
             sampleData.join('')
         ].join('');
-
-        //
-        // Header + Sub-Chunks
-        //
 
         var data = [
             'RIFF',
@@ -172,9 +103,6 @@ window.log = function f(){ log.history = log.history || []; log.history.push(arg
         ].join('');
 
         if (canBlob) {
-            // so chrome was blowing up, because it just blows up sometimes
-            // if you make dataURIs that are too large, but it lets you make
-            // really large blobs...
             var view = new Uint8Array(data.length);
             for (var i = 0; i < view.length; i++) {
                 view[i] = data.charCodeAt(i);
